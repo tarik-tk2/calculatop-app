457f1fb9eaabf140c6df3fbeba67339ea5394193
tarik <mahimtk2@gmail.com>
2023-01-09 09:07:37 +0600

    hello

--- indext.js
+++ indext.js
@@ -14,9 +14,7 @@
 //   clear()
 // });
 
-
-
-// delete  number 
+// delete  number
 // const deleteId = document.getElementById("delet");
 // deleteId.addEventListener("click", () => {
 //     if (displayValue > 0) {
@@ -24,15 +22,15 @@
 //         displayDigitSplit.pop()
 //         const remainDisplayDigit=displayDigitSplit.join('')
 //         displayId.innerText = remainDisplayDigit;
-//         if (displayDigitSplit.length === 0) { 
+//         if (displayDigitSplit.length === 0) {
 //             clear()
 //         }
-//     }     
+//     }
 // })
 // //append number
-// document.querySelectorAll('[data-number]').forEach(number => { 
+// document.querySelectorAll('[data-number]').forEach(number => {
 //     number.addEventListener("click", (e) => {
-//         if (displayValue) { 
+//         if (displayValue) {
 //             displayId.innerText=displayId.innerText+e.target.innerText
 //         }
 //     })
@@ -42,11 +40,42 @@
 //     operation.addEventListener('click', (e) => {
 //         if (displayValue ) {
 //             displayId.innerText = displayId.innerText + e.target.innerText;
-//             if (e.target.innerText === "*") { 
-                
+//             if (e.target.innerText === "*") {
+
 //             }
 //         }
-      
+
 //    })
 // })
 
+function allData() {
+  let screen = document.getElementById("display");
+  const button = document.querySelectorAll("[data-number]");
+  const allClear = document.getElementById("allClear");
+    let result = document.getElementById("equal");
+    let del = document.getElementById("delet")
+  
+
+  button.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      let value = e.target.innerText;
+      screen.innerText += value;
+    });
+  });
+    result.addEventListener("click", (e) => {
+        if (screen.innerText === '') {
+            screen.innerText = "";
+        } else {
+            let answer = eval(screen.innerText)
+            screen.innerText=answer
+         }
+    })
+    del.addEventListener("click", (e) => { 
+        let value = (screen.innerText).substring(0, (screen.innerText.toString().length - 1))
+        screen.innerText=value
+    })
+    allClear.addEventListener("click", (e) => {
+       screen.innerText=''
+    })
+}
+allData();
